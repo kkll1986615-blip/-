6ae76f2b1085d3b1c0e47f873feac9dc65f7fa2b
kkll1986615-blip <kkll1986615@gmail.com>
2026-09-10 12:18:56 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE = 'sports-platform-v2-fast-sync';
+const CORE = ['./', './index.html', './manifest.webmanifest', './icons/icon-192.png', './icons/icon-512.png', './icons/apple-touch-icon.png', './icons/favicon-48.png'];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  const url = new URL(event.request.url);
+  if (url.origin !== self.location.origin) return;
+  event.respondWith(fetch(event.request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE).then(cache => cache.put(event.request, copy)).catch(()=>{});
+    return response;
+  }).catch(() => caches.match(event.request).then(r => r || caches.match('./index.html'))));
+});
