0041fbff7e2c16f5b4a3063f4eaf8528850cb29f
kkll1986615-blip <kkll1986615@gmail.com>
2026-09-15 07:36:35 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,20 +1,32 @@
-const CACHE='almanar-sports-2026-09-14-1';
-const CORE=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png','./icons/favicon-48.png','./icons/apple-touch-icon.png'];
-
-self.addEventListener('install',event=>{
-  event.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()));
+const CACHE_NAME = "almanar-sport-2026-09-15-v2";
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icons/favicon-48.png",
+  "./icons/apple-touch-icon.png",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
 });
-self.addEventListener('activate',event=>{
-  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+self.addEventListener("activate", event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim()));
 });
-self.addEventListener('fetch',event=>{
-  const req=event.request;
-  if(req.method!=='GET') return;
+self.addEventListener("fetch", event => {
+  const req = event.request;
+  if (req.method !== "GET") return;
   event.respondWith(
-    fetch(req).then(res=>{
-      const copy=res.clone();
-      caches.open(CACHE).then(c=>c.put(req,copy)).catch(()=>{});
-      return res;
-    }).catch(()=>caches.match(req).then(r=>r||caches.match('./index.html')))
+    caches.match(req).then(cached => {
+      const network = fetch(req).then(res => {
+        if (res && res.ok && new URL(req.url).origin === self.location.origin) {
+          const copy = res.clone();
+          caches.open(CACHE_NAME).then(c => c.put(req, copy)).catch(()=>{});
+        }
+        return res;
+      }).catch(() => cached);
+      return cached || network;
+    })
   );
 });
